79fc9e60a20785bc1661aef56cb29991f6f66d2c
pridvorov <pridvorov@gmail.com>
2019-11-08 17:57:22 +0700

    working

--- src/main/java/bd/Connect_bd.java
+++ src/main/java/bd/Connect_bd.java
@@ -5,57 +5,67 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
+import java.util.logging.Logger;
 
 public class Connect_bd {
 
-    private static final String DB_DRIVER="org.firebirdsql.jdbc.FBDriver";
-    private static final String DB_URL="jdbc:firebirdsql:localhost:E:\\runliwest\\base\\30062019\\LIWEST.FDB";
-    private static Connection connection = null;
-    private static Statement statement = null;
+    private static final String DB_DRIVER = "org.firebirdsql.jdbc.FBDriver";
+    private static final String DB_URL = "jdbc:firebirdsql:localhost:E:\\runliwest\\base\\work\\LIWEST.FDB";
+    private static Connection connection =  null;
+    private static boolean flag = false;
 
+     private Logger logger;
 
-    public static boolean Connect_bd() throws ClassNotFoundException {
+    public Connect_bd() throws ClassNotFoundException {
 
         try {
             Class.forName(DB_DRIVER);
         } catch (ClassNotFoundException e) {
-            System.out.println("connectToDB: Firebird JCA-JDBC драйвер не найден");
+            logger.info("connectToDB: Firebird JCA-JDBC драйвер не найден");
         }
+
         try {
-             String conString = DB_URL;
-             Properties paramConnection = new Properties();
-             paramConnection.setProperty("user", "SYSDBA");
-             paramConnection.setProperty("password", "ktybdsq,fnjy");
-             paramConnection.setProperty("encoding", "WIN1251");
-             System.out.println(DriverManager.getConnection(conString, paramConnection).getCatalog().toString());
-             connection =  DriverManager.getConnection(conString, paramConnection);
-             return true;
-            }
+            String conString = DB_URL;
+            Properties paramConnection = new Properties();
+            paramConnection.setProperty("user", "SYSDBA");
+            paramConnection.setProperty("password", "masterkey1");
+            paramConnection.setProperty("encoding", "WIN1251");
+            connection = DriverManager.getConnection(conString, paramConnection);
+            flag = true;
+            //logger.info("Подключены к базе" + conString.toString());
+        }
         catch (SQLException e1) {
             e1.printStackTrace();
-            System.out.println("Не подключены к базе"+e1.toString());
-            return false;
-            }
+             logger.info("Не подключены к базе" + e1.toString());
+        }
     }
 
-    public  Connection getConnection() {
+    public Connection getConnection() {
         return connection;
     }
 
-    public static Statement getStatementForFBConnect(){
+
+    public Statement getStatementForFBConnect(){
         Statement statement;
         try {
             statement = connection.createStatement();
         } catch (SQLException e) {
-            System.out.println("Не создан staitment : "+e.getMessage());
+            //logger.info("AccessToFB: "+e.getMessage());
             return null;
         }
         return statement;
     }
 
 
+    public boolean closeAccess() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
-
-   }
+}
 
--- src/main/java/post/GetPost.java
+++ src/main/java/post/GetPost.java
@@ -1,6 +1,11 @@
 package post;
 
+import bd.Connect_bd;
 import connect.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.sql.*;
@@ -8,6 +13,8 @@ import java.text.SimpleDateFormat;
 
 import java.util.Properties;
 import java.util.StringTokenizer;
+
+import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.http.client.ClientProtocolException;
 
 import org.json.JSONArray;
@@ -16,6 +23,7 @@ import org.json.JSONObject;
 import java.time.*;
 import java.time.format.*;
 import java.time.temporal.*;
+import java.util.logging.Logger;
 
 
 public class GetPost {
@@ -41,56 +49,62 @@ public class GetPost {
         return FORMATTER.parseBest(dateAsString, LocalDate::from, YearMonth::from, Year::from);
     }
 
-    public static boolean isValidDate(String dateAsString) {
+
+
+    public static void main(String[] args) throws ClientProtocolException, IOException, ClassNotFoundException,SQLException {
+
+        Statement statement = null;
+
         try {
-            parseDate(dateAsString);
-            return true;
-        } catch (DateTimeParseException e) {
-            return false;
+             statement = new Connect_bd().getStatementForFBConnect();
         }
-    }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
-
-    public static  void main(String[] args) throws ClientProtocolException, IOException, ClassNotFoundException, SQLException {
         JSONArray array = new JSONArray();
+        boolean test = false;
+        //if (statement) logger.info("");
+
         /**Запрос JSON*/
         //try {
-        array = Getpostsite.postFromSiteToJSON("https://liwest.ru/partners-app/all_partners_to_xml.php?get_partner=no_transfer&pass=GjKeXbNm;");
-        System.out.println("Начинаем работать date:"+LocalDateTime.now().toString());
-        if (array.toString() != "[]"){
-             String DB_DRIVER="org.firebirdsql.jdbc.FBDriver";
-             String DB_URL="jdbc:firebirdsql:LAO:D:\\LiWest\\Bases\\LiWest.fdb";
-             //String DB_URL="jdbc:firebirdsql:localhost:E:\\runliwest\\base\\works\\LIWEST.FDB";
-             Connection connFire = null;
-        // Connection connFire1 = null;
-        //if (new Connect_bd().getConnection()) ;
-              try {
-                  Class.forName(DB_DRIVER);
-                     } catch (ClassNotFoundException e)
-                {
-                    System.out.println("connectToDB: Firebird JCA-JDBC драйвер не найден");
-                }
-               try {
-                 Properties paramConnection = new Properties();
-                 paramConnection.setProperty("user", "SYSDBA");
-                 paramConnection.setProperty("password", "ktybdsq,fnjy");
-                 paramConnection.setProperty("encoding", "WIN1251");
-                 connFire = DriverManager.getConnection(DB_URL, paramConnection);
+        // "[{"birthday":"24.06.1995","sponsor":null,"address":"Ростов, ул.Ленина, 67-76","flag":"N","sponsor_code":"85858555","phone":"+7-454-455-64-78","isc_address":"Тюмень, ИСЦ Пети","site_id":"15519","name":"Тестовик Тестик Тест","email":"goro@mail.ru"}]";
+        //array = Getpostsite.postFromSiteToJSON("https://liwest.ru/partners-app/all_partners_to_xml.php?get_partner=no_transfer&pass=GjKeXbNm;");
+
+        try {
+              File file = new File("d:\\jsontest.txt");
+              FileReader fileReader = new FileReader(file); // поток, который подключается к текстовому файлу
+              BufferedReader bufferedReader = new BufferedReader(fileReader); // соединяем FileReader с BufferedReader
+              String line;
+              while((line = bufferedReader.readLine()) != null) {
+                array = new JSONArray(line); // выводим содержимое файла на экран построчно
             }
-        catch (SQLException e1) {
-            e1.printStackTrace();
+            bufferedReader.close(); // закрываем поток
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject object = array.getJSONObject(i);
-            /**Разбивка name на фамилию имя отчество
-             * и заливли в массив*/
-            String[] lastFirstMiddle= new String[3];
-            int number=0;
-            StringTokenizer st = new StringTokenizer(object.getString("name").toString(), " ");
-            while(st.hasMoreTokens()){
-                  lastFirstMiddle[number]= st.nextToken();
-                  number++;
-            }
+
+        System.out.println(array);
+
+
+
+
+
+       System.out.println("Начинаем работать date:"+LocalDateTime.now().toString());
+
+        if (array.toString() != "[]"){
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                /**Разбивка name на фамилию имя отчество
+                 * и заливли в массив*/
+                String[] lastFirstMiddle= new String[3];
+                int number=0;
+                StringTokenizer st = new StringTokenizer(object.getString("name").toString(), " ");
+                while(st.hasMoreTokens()){
+                      lastFirstMiddle[number]= st.nextToken();
+                      number++;
+                }
+
             if (lastFirstMiddle[2]==null)
                    {lastFirstMiddle[2]=" ";};
 
@@ -103,11 +117,12 @@ public class GetPost {
              sqlInDistributors.append(" and upper(firstname)=upper(trim(").append("\'").append(lastFirstMiddle[1].toString().toUpperCase()).append("\'").append("))");
              //sqlInDistributors.append(" and upper(middlename)=upper(trim(").append("\'").append(lastFirstMiddle[2].toString().toUpperCase()).append("\'").append("))");
              sqlInDistributors.append(" and sourcesponsor in (select id from distributors where code=").append("\'").append(object.getString("sponsor_code").toString()).append("\')");
-             /*sqlInDistributors.append(" and birthdate=").append("\'").append(object.getString("birthday").toString()).append("\'");*/
+             sqlInDistributors.append(" and birthdate=").append("\'").append(object.getString("birthday").toString()).append("\'");
              //System.out.println(sqlInDistributors.toString());
 
-            Statement stm= connFire.createStatement();
-            ResultSet res=stm.executeQuery(sqlInDistributors.toString());
+             //Statement stm = Connect_bd.Connect(); //connFire.createStatement();
+
+            ResultSet res = statement.executeQuery(sqlInDistributors.toString());
 
             String code = "";
             int count=-1;
@@ -124,8 +139,8 @@ public class GetPost {
             String sponsoremail = "";
 
             StringBuilder sqlIdSponsor = new StringBuilder("select id sponsor,email sponsoremail from distributors where code=").append("\'").append(object.getString("sponsor_code").toString()).append("\'");
-            Statement getIdSponsor  = connFire.createStatement();
-            ResultSet idSponsor = getIdSponsor.executeQuery(sqlIdSponsor.toString());
+            //Statement getIdSponsor  = Connect_bd.Connect();
+            ResultSet idSponsor = statement.executeQuery(sqlIdSponsor.toString());
             while (idSponsor.next()) {
                 sponsor = idSponsor.getInt("sponsor");
                 if (sponsoremail!=null) {
@@ -139,13 +154,13 @@ public class GetPost {
              }
 
              if (id>0) {
-                 /**Если записи есть то пометим их в bitrix*/
+                 /**Если записи есть то пометим их в bitrix
                     Getpostsite.postToSite(new StringBuilder("https://liwest.ru/partners-app/check_partner_xml.php?check_partner=").append(object.getInt("site_id")).append("&code=").append(code).append("&pass=PfUhE;Ty;").append("&sponsoremail=").append(sponsoremail).toString());
                     System.out.printf(new StringBuilder("https://liwest.ru/partners-app/check_partner_xml.php?check_partner=").append(object.getInt("site_id")).append("&code=").append(code).append("&pass=PfUhE;Ty;").append("&sponsoremail=").append(sponsoremail).toString());
-                  /****/
-                 System.out.println(new StringBuilder("site_id=").append(code).append(" уже есть в базе ").append(LocalDateTime.now().toString()));
+                    System.out.println(new StringBuilder("site_id=").append(code).append(" уже есть в базе ").append(LocalDateTime.now().toString()));
+                  */
              } else {
-                 if (connFire != null) {
+                 if (statement != null) {
 
                      /**Код договора*/
                      count= -1;
@@ -157,8 +172,8 @@ public class GetPost {
                       * Готовим код договора если в базе есть договор с 555* то добавляем к нему 1 если нет договора то номером будет 555000001
                        */
                      StringBuilder sqlMaxCode = new StringBuilder("select max(code) code from distributors where code like ").append("\'").append(prexixDogovora).append("\'");
-                     Statement stm555 = connFire.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                     ResultSet resMaxCode = stm555.executeQuery(sqlMaxCode.toString());
+                     //Statement stm555 = Connect_bd.Connect();/*createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);*/
+                     ResultSet resMaxCode = statement.executeQuery(sqlMaxCode.toString());
                      //System.out.println(resMaxCode == null);
                      //System.out.println(resMaxCode.getMetaData().getColumnCount());
                      while (resMaxCode.next()) {
@@ -173,8 +188,8 @@ public class GetPost {
                      /**
                       * Готовим вставку в базу дистрибьютера генерим id и добавляем в object
                       * */
-                     Statement genKeyIdDistribution  = connFire.createStatement();
-                     ResultSet keyValue = genKeyIdDistribution.executeQuery("select gen_id(DISTRIBUTOR_gen, 1) from  RDB$DATABASE");
+                     //Statement genKeyIdDistribution  = Connect_bd.Connect();
+                     ResultSet keyValue = statement.executeQuery("select gen_id(DISTRIBUTOR_gen, 1) from  RDB$DATABASE");
                      Integer keyIdDistributor = 0;
                      while (keyValue.next()) {
                             keyIdDistributor = keyValue.getInt("GEN_ID");
@@ -184,11 +199,11 @@ public class GetPost {
 
                      StringBuilder objectsInsert = new StringBuilder("INSERT INTO OBJECTS (BRANCH, ID, OBJ_TYPE, NAME) VALUES ");
                      objectsInsert.append("(").append("999").append(",").append(keyIdDistributor.toString()).append(",100").append(",\' \'").append(")");
-                     Statement stmObjectsInsert = connFire.createStatement();
-                     /**Вставка в object*/
-                      boolean resstmObjectsInsert = stmObjectsInsert.execute(objectsInsert.toString());
-                      /**
-                      */
+                     //Statement stmObjectsInsert = Connect_bd.Connect();
+                     /**Вставка в object  убирать коменты */
+
+                     boolean resstmObjectsInsert = statement.execute(objectsInsert.toString());
+
 
                      /**Готовим вставку в DISTRIBUTORS id берем из предыдущей генерации для связи реляций*/
 
@@ -199,18 +214,18 @@ public class GetPost {
                      distributorsInsert.append("BIRTHDATE, ZIPCODE, COUNTRY, REGION, CITY, ADDRESS, PASSPORT,CREATE_DATE, CREATE_BY, REGDATE, SOURCESPONSOR, EDIT_DATE,");
                      distributorsInsert.append("DELETEDATE, BRANCH, ID_BRANCH, DECLAREDSTATUS, PHONE3, EMAIL) VALUES (");
                      distributorsInsert.append(keyIdDistributor).append(",");;//id
-                     distributorsInsert.append("\'").append(lastFirstMiddle[0].toString()).append("\',");//lastname
-                     distributorsInsert.append("\'").append(lastFirstMiddle[1].toString()).append("\',");//firstname
-                     distributorsInsert.append("\'").append(lastFirstMiddle[2].toString()).append("\',");//middlename
+                     distributorsInsert.append("\'").append(lastFirstMiddle[0].toString().toUpperCase()).append("\',");//lastname
+                     distributorsInsert.append("\'").append(lastFirstMiddle[1].toString().toUpperCase()).append("\',");//firstname
+                     distributorsInsert.append("\'").append(lastFirstMiddle[2].toString().toUpperCase()).append("\',");//middlename
                      distributorsInsert.append("\'").append(newCode).append("\',");//code
                      distributorsInsert.append("\'").append("\',");//phone1
-                     distributorsInsert.append("\'").append("\',");//pfone2
+                     distributorsInsert.append("\'").append("\',");//phone2
                      distributorsInsert.append("\'").append(object.getString("birthday")).append("\',");
                      distributorsInsert.append("\'").append("\',");//ZIPCODE
                      distributorsInsert.append("\'").append("\',");//COUNTRY
                      distributorsInsert.append("\'").append("\',");//REGION
                      distributorsInsert.append("\'").append("\',");//CITY
-                     distributorsInsert.append("\'").append("\',");//ADDRESS
+                     distributorsInsert.append("\'").append(object.getString("address").toString()).append("\',");;//ADDRESS
                      distributorsInsert.append("\'").append("\',");//PASSPORT
                      distributorsInsert.append("\'").append(datenow.toString()).append("\',");//CREATE_DATE
                      distributorsInsert.append("\'").append("1").append("\',");//CREATE_BY
@@ -221,30 +236,28 @@ public class GetPost {
                      distributorsInsert.append("\'").append("1").append("\',");//BRANCH
                      distributorsInsert.append("\'").append("999").append("\',");//ID_BRANCH
                      distributorsInsert.append("NULL").append(",");//DECLAREDSTATUS
-                     distributorsInsert.append("\'").append("\',");//PHONE3
+                     distributorsInsert.append("\'").append(object.getString("phone")).append("\',");//pfone3
                      distributorsInsert.append("\'").append(object.getString("email").toString()).append("\')");//email
 
-                     Statement stmDistributorsInsert = connFire.createStatement();
-                     /**Вставка в Distributor*/
-                     boolean resDistributorsInsert = stmDistributorsInsert.execute(distributorsInsert.toString());
-                      /***/
+                     //Statement stmDistributorsInsert = Connect_bd.Connect();
 
-                     /**Обновляем инфу в object
-                      *  */
+                     System.out.println(distributorsInsert.toString());
+                     /**Вставка в Distributor убирать коменты*/
+                     boolean resDistributorsInsert = statement.execute(distributorsInsert.toString());
                      StringBuilder objectsUpdate = new StringBuilder("UPDATE objects SET name=");
                      objectsUpdate.append("\'").append(newCode).append(" ").append(lastFirstMiddle[0].toString()).append(" ").append(lastFirstMiddle[1].substring(1, 2).toUpperCase()).append(".").append(".\'");
                      objectsUpdate.append(" where BRANCH = 999 and ID =").append(keyIdDistributor).append(" and OBJ_TYPE=100");
-                     Statement stmobjectsUpdate = connFire.createStatement();
+                     //Statement stmobjectsUpdate = Connect_bd.Connect();
 
+                     /**Обновляем инфу в object убирать коменты*/
+                      boolean resobjectsUpdate = statement.execute(objectsUpdate.toString());
 
-                      boolean resobjectsUpdate = stmDistributorsInsert.execute(objectsUpdate.toString());
-
-                     /**Пометили в bitrix*/
+                     /**Пометили в bitrix убирать коменты*/
                      System.out.println(new StringBuilder("https://liwest.ru/partners-app/check_partner_xml.php?check_partner=").append(object.getInt("site_id")).append("&code=").append(newCode).append("&pass=PfUhE;Ty;").append("&sponsoremail=").append(sponsoremail).toString());
                      System.out.println("Добавили а базу и отправили в bitrix c кодом договора "+newCode.toString()+" date:"+LocalDateTime.now().toString());
                      Getpostsite.postToSite(new StringBuilder("https://liwest.ru/partners-app/check_partner_xml.php?check_partner=").append(object.getInt("site_id")).append("&code=").append(newCode).append("&pass=PfUhE;Ty;").append("&sponsoremail=").append(sponsoremail).toString());
                      //Getpostsite.postToSite(new StringBuilder("https://liwest.ru/partners-app/check_partner_xml.php?check_partner=").append(object.getInt("site_id")).append("&code=").append(newCode).append("&pass=PfUhE;Ty;").append("&sponsoremail=").append("support@liwest.ru").toString());
-                     /***/
+
                  } else System.out.println("Нет связи с БД");
              }
         }
@@ -252,8 +265,11 @@ public class GetPost {
            ex.printStackTrace();
         }*/
     }
+        //cbd.closeAccess();
+
+        }
 }
-}
+
 
 
 
